cb8587423060ef7351abd702128a4393a685b798
khaled <khaledbahjat0@gmail.com>
2024-09-17 21:48:38 +0300

    VSCODE

--- Template.cpp
+++ Template.cpp
@@ -1,48 +1,98 @@
+#include <cmath>
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define cin(vec) for(auto& i : vec) cin >> i
-#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
-#define cout(vec) for(auto& i : vec) cout << i << " "; cout << "\n";
-#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
-#define cout_map(mp) for(auto& [f, s] : mp) cout << f << "  " << s << "\n";
-#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
-#define fixed(n) fixed << setprecision(n)
-#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
-#define fill(vec, value) memset(vec, value, sizeof(vec));
-#define Num_of_Digits(n) ((int)log10(n) + 1)
-#define mod_combine(a, b, m) (((a % m) * (b % m)) % m)
-#define all(vec) vec.begin(), vec.end()
-#define rall(vec) vec.rbegin(), vec.rend()
-#define sz(x) int(x.size())
-#define debug(x) cout << #x << ": " << (x) << "\n";
-#define fi first
-#define se second
-#define Pair pair < int, int >
-#define ll long long
-#define ull unsigned long long
-#define Mod  1'000'000'007
-#define OO 2'000'000'000
-#define EPS 1e-9
-#define PI acos(-1)
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
-void Fast_IO(){
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
-    #endif
+namespace __gnu_pbds {
+    typedef tree< long long ,
+        null_type ,
+        less< long long > ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update > ordered_set;
 }
 
-void Solve(){
-    
-}
+using namespace __gnu_pbds;
 
-int main(){
-    Fast_IO();
-    int t = 1;
-    //cin >> t;
-    while(t--)
-        Solve();
+
+#define iter iterator
+#define Khaled std::ios::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+
+typedef long long ll;
+
+typedef pair< int , int > pi;
+typedef pair< int , pi > p3i;
+
+typedef pair< ll , ll > pll;
+
+typedef vector< int > vi;
+typedef vector< pi > v2i;
+
+typedef vector< string > vs;
+typedef vector< bool > vb;
+
+typedef vector< vi > vvi;
+
+typedef priority_queue< int > pq_max;
+typedef priority_queue< int , vi , greater< int > > pq_min;
+
+#define MP make_pair
+#define PB push_back
+#define EmB emplace_back
+#define Em emplace
+#define len(s) (ll) s.length()
+#define sz(v) (ll) v.size()
+#define all(a) a.begin(), a.end()
+#define all_r(a) a.rbegin(), a.rend()
+#define mem(x, val) memset((x), (val), sizeof(x))
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define Product(v, ans) cout << accumulate(all(v), ans, multiplies<int>());
+
+#define tst     \
+    ll t;       \
+    cin >> t;   \
+    ll i_t = 0; \
+    while (i_t++ < t)
+#define SetPre(n, x) cout << fixed << setprecision(n) << x
+
+#define int long long
+
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define F0R(i, a) for (int i = 0; i < a; i++)
+#define F1R(i, a) for (int i = 1; i <= a; i++)
+#define cin(v)        \
+    for (auto &i : v) \
+        cin >> i;
+
+#define cout(v)      \
+    for (auto i : v) \
+        cout << i << " ";
+
+#define el '\n'
+/*============================== Glopal Memory ============================*/
+
+/********************************  FUNCTIONS  *****************************/
+
+//********************************   MAIN  *********************************/
+void NoSt0n_ ( int tc );
+
+signed main ( ) {
+    Khaled 
+    int _ = 1;
+    // cin >> _;
+    for (int __ = 1; __ <= _; __++){
+        // cout << "Case #" << __ << " : ";
+        NoSt0n_(__);
+        if (__ < _){
+            cout << '\n';
+        }
+    }
     return 0;
-} 
+}
+
+/*=================================== NoSt0n_ =============================*/
+void NoSt0n_ ( int tc ) {
+
+}
